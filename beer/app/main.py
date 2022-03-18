9a65322943ef1f5a6ed6fb6cf49f941c323c5622
DvaMishkiLapa <artem132rus@gmail.com>
2022-03-18 17:15:47 +0300

    Add simply logger messages

--- beer/app/main.py
+++ beer/app/main.py
@@ -3,12 +3,13 @@ import asyncio
 import aiohttp
 from prometheus_client import Counter
 from sanic import Sanic
+from sanic.log import logger
 from sanic.response import json
 from sanic_openapi import openapi3_blueprint
 
 counter = Counter("sanic_requests_total",
-                             "Track the total number of requests",
-                             ["method"])
+                  "Track the total number of requests",
+                  ["method"])
 
 
 app = Sanic(name='beer')
@@ -19,6 +20,8 @@ beer_url = "https://random-data-api.com/api/beer/random_beer"
 @app.middleware('request')
 async def track_requests(request):
     counter.labels(method=request.method).inc()
+    count_request = counter.labels(method=request.method)._value.get()
+    logger.debug(f'track_requests inc requests counter: {count_request}')
 
 
 @app.listener('before_server_start')
@@ -41,8 +44,10 @@ async def get_beer(request):
     for x in range(5):
         tasks.append(asyncio.ensure_future(get_beer(app.ctx.httpSession, beer_url)))
     beers = await asyncio.gather(*tasks)
+    logger.debug(f'get_beer get dirty beer data. Len: {len(beers)}')
     ans = [{k: b[k] for k in ['brand', 'name', 'alcohol']} for b in beers]
     ans.sort(key=extract_alcohol_float)
+    logger.debug(f'get_beer get clear sort beer data. Len: {len(ans)}')
     return json(ans)
 
 
@@ -52,11 +57,14 @@ async def get_stats(request):
     Метод показывает сколько запросов было на сервер (с момента запуска)
     '''
     count_request = counter.labels(method=request.method)._value.get()
+    logger.debug(f'count_request get count request: {count_request}')
     return json([{'count_request': count_request}])
 
 
 def extract_alcohol_float(beers_list):
-    return float(beers_list['alcohol'][:-1])
+    out = float(beers_list['alcohol'][:-1])
+    logger.debug(f'extract_alcohol_float {beers_list} => {out}')
+    return out
 
 
 async def get_beer(session, url):
